5e3a7f1eb54e3bfe11fe82b334bfb46696956a7d
Dmitry Seloustev <nimec77@gmail.com>
2025-08-29 22:22:12 +0700

    Refactor configuration handling and project structure
    - Added library and binary paths in `Cargo.toml`.
    - Introduced constants for environment variable keys in `app_config.rs`.
    - Enhanced error messages for loading configuration.
    - Added unit tests for `AppConfig` to validate configuration loading.

--- src/app_config.rs
+++ src/app_config.rs
@@ -2,6 +2,11 @@ use std::path::{Path, PathBuf};
 
 use secrecy::{ExposeSecret, SecretBox, SecretString};
 
+pub const TG_ID_KEY: &str = "TG_ID";
+pub const TG_HASH_KEY: &str = "TG_HASH";
+pub const TG_PHONE_KEY: &str = "TG_PHONE";
+pub const TG_SESSION_FILE_PATH_KEY: &str = "TG_SESSION_FILE_PATH";
+
 #[derive(Debug, Clone)]
 pub struct AppConfig {
     tg_id: SecretBox<i32>,
@@ -28,16 +33,23 @@ impl AppConfig {
     }
 
     pub fn load_config() -> Result<Self, anyhow::Error> {
-        let tg_id = dotenvy::var("TG_ID")
-            .expect("TG_ID must be set")
-            .parse::<i32>()
-            .expect("TG_ID must be a valid integer");
-        let tg_hash = dotenvy::var("TG_HASH").expect("TG_HASH must be set");
-        let tg_phone = dotenvy::var("TG_PHONE").expect("TG_PHONE must be set");
-        let tg_session_file_path = dotenvy::var("TG_SESSION_FILE_PATH")
-            .expect("TG_SESSION_FILE_PATH must be set")
-            .into();
+        log::debug!("Loading config");
 
+        let must_tg_id = format!("{TG_ID_KEY} must be set");
+        let must_tg_id_integer = format!("{TG_ID_KEY} must be a valid integer");
+        let tg_id = dotenvy::var(TG_ID_KEY)
+            .expect(&must_tg_id)
+            .parse::<i32>()
+            .expect(&must_tg_id_integer);
+
+        let must_tg_hash = format!("{TG_HASH_KEY} must be set");
+        let tg_hash = dotenvy::var(TG_HASH_KEY).expect(&must_tg_hash);
+        let must_tg_phone = format!("{TG_PHONE_KEY} must be set");
+        let tg_phone = dotenvy::var(TG_PHONE_KEY).expect(&must_tg_phone);
+        let must_tg_session_file_path = format!("{TG_SESSION_FILE_PATH_KEY} must be set");
+        let tg_session_file_path = dotenvy::var(TG_SESSION_FILE_PATH_KEY)
+            .expect(&must_tg_session_file_path)
+            .into();
 
         let config = Self {
             tg_id: SecretBox::new(Box::new(tg_id)),
@@ -45,8 +57,54 @@ impl AppConfig {
             tg_phone: SecretString::new(tg_phone.into_boxed_str()),
             tg_session_file_path,
         };
-        log::info!("Config loaded: {:?}", config);
+        log::debug!("Config loaded: {:?}", config);
 
         Ok(config)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env::{remove_var, set_var};
+
+    const TG_ID: &str = "1234567890";
+    const TG_HASH: &str = "aslkdfjsdklfjsdklfjklsdjflksjdf";
+    const TG_PHONE: &str = "+79111234567";
+    const TG_SESSION_FILE_PATH: &str = "/tmp/tg_session.json";
+
+    fn setup_env() {
+        unsafe {
+            set_var(TG_ID_KEY, TG_ID);
+            set_var(TG_HASH_KEY, TG_HASH);
+            set_var(TG_PHONE_KEY, TG_PHONE);
+            set_var(TG_SESSION_FILE_PATH_KEY, TG_SESSION_FILE_PATH);
+        };
+    }
+
+    fn teardown_env() {
+        unsafe {
+            remove_var(TG_ID_KEY);
+            remove_var(TG_HASH_KEY);
+            remove_var(TG_PHONE_KEY);
+            remove_var(TG_SESSION_FILE_PATH_KEY);
+        };
+    }
+
+    #[test]
+    fn test_load_config() {
+        setup_env();
+
+        let config = AppConfig::load_config().expect("Failed to load config");
+
+        teardown_env();
+
+        assert_eq!(config.tg_id(), TG_ID.parse::<i32>().unwrap());
+        assert_eq!(config.tg_hash(), TG_HASH);
+        assert_eq!(config.tg_phone(), TG_PHONE);
+        assert_eq!(
+            config.tg_session_file_path().to_string_lossy(),
+            TG_SESSION_FILE_PATH
+        );
     }
 }
