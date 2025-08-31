21338a1872466bf8aa2cdb449f8b59f29f4a5311
Dmitry Seloustev <nimec77@gmail.com>
2025-08-31 14:20:28 +0700

    Add async-trait dependency and implement TelegramClient trait
    - Added `async-trait` as a dependency in `Cargo.toml` and updated `Cargo.lock`.
    - Introduced `error` module in `lib.rs` for better error handling.
    - Defined `TelegramClient` trait in `telegram.rs` to facilitate asynchronous operations with Telegram configuration.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod telegram;
 
 pub mod app_config;
+
+pub mod error;
--- src/telegram.rs
+++ src/telegram.rs
@@ -1,3 +1,7 @@
+use async_trait::async_trait;
+
+use crate::app_config::TelegramConfig;
+
 pub mod create_session;
 
 pub mod close_session;
@@ -5,3 +9,11 @@ pub mod close_session;
 pub mod get_channels_list;
 
 pub mod get_new_messages;
+
+
+#[async_trait]
+pub trait TelegramClient {
+    fn new(config: &TelegramConfig) -> Self;
+    // fn get_channels_list(&self) -> Result<Vec<Channel>, anyhow::Error>;
+    // fn get_new_messages(&self) -> Result<(), anyhow::Error>;
+}
